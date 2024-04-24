286fbd4be916517a840cb69d403972d7098cb290
Gabriel Marmitt <gabrielm.linassi@gmail.com>
2024-04-24 18:05:27 -0300

    update cv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,95 @@
 
 import { forwardRef, useRef } from "react"
 import { useReactToPrint } from "react-to-print"
-import { PrinterIcon } from "lucide-react"
+import { DownloadIcon, PrinterIcon } from "lucide-react"
 
 import SkillsSection from "@/components/Skills"
 import { SKILLS } from "@/data/Skills"
 import { LANGUAGES } from "@/data/Languages"
+import dayjs from "dayjs"
+
+const experiences = [
+  {
+    startAt: "2022-03",
+    role: "Mobile & Frontend Web Developer",
+    company: "Suvie · Full-time",
+    location: "Boston (Remote)",
+    description: [
+      "Refactoring company's site from CSR to SSG. SEO and lighthouse scores were hugely improved",
+      "Merge various projects into a monorepo (Turborepo) to allow code sharing. Resulted in better DX and DRY code",
+      "Maintenance of mobile app built in React Native for controlling an IOT device",
+      "Created a solution for handling A/B testing without giving up on SSG",
+    ],
+  },
+  {
+    startAt: "2021-05",
+    endAt: "2022-02",
+    role: "Frontend Web Developer",
+    company: "SweetRush Inc · Full-time",
+    location: "San Francisco (Remote)",
+    description: [
+      "Refactoring a Shopify store to use Hydrogen React",
+      "Development of landing pages for marketing campaigns",
+      "Migration of site from WordPress to Headless WP + Next.js",
+    ],
+  },
+  {
+    startAt: "2020-01",
+    endAt: "2021-03",
+    role: "Frontend Web Developer",
+    company: "PatientStudio · Full-time",
+    location: "Brazil",
+    description: [
+      "Development of a clinical application for managing patient data, appointment scheduling, and health issue tracking",
+    ],
+  },
+  {
+    startAt: "2018-08",
+    endAt: "2019-12",
+    role: "Full Stack Web Developer",
+    company: "UpWork",
+    location: "Freelancer",
+    description: [
+      "Development of various sites and applications",
+      "Migration of a legacy app built with Redux and complex state to a simpler architecture in ReactQuery",
+      "Performance improvement by replacing MUI with Tailwind + Radix. Lighhouse scores more than doubled",
+    ],
+  },
+  {
+    startAt: "2017-04",
+    endAt: "2018-07",
+    role: "Desktop Developer",
+    company: "Prevedello Sistemas",
+    location: "Brazil",
+    description: ["Development of ERP systems", "API integrations with warehouse and e-commerces"],
+  },
+]
+
+const description =
+  "Front-End Engineer with a passion for building innovative products and user interfaces using cutting edge web technologies. Over 6 years of experience creating modern and performant web applications. Proficient in Javascript, TypeScript, React and Next.js. Also learning Node and React Native."
+
+const getWorkingTime = (startAt: string, endAt?: string) => {
+  try {
+    const diff = dayjs(endAt).diff(startAt, "M")
+    const years = Math.floor(diff / 12)
+    const months = (diff + 1) % 12
+    return { years, months }
+  } catch (error) {
+    return {}
+  }
+}
+
+const getWorkingTimeFormatted = (startAt: string, endAt?: string) => {
+  try {
+    const { years, months } = getWorkingTime(startAt, endAt)
+
+    let msg = ""
+    if (years) msg += `${years}yr `
+    if (months) msg += `${months}mos`
+
+    return msg
+  } catch (error) {}
+}
 
 export default function Home() {
   const cvRef = useRef<HTMLDivElement>(null)
@@ -17,7 +101,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-gray-100">
-      <nav className="border-b bg-white">
+      {/* <nav className="border-b bg-white">
         <div className="mx-auto flex h-16 w-full max-w-4xl items-center justify-end px-2">
           <button
             className="flex items-center space-x-2 rounded bg-blue-500 py-2 px-4 font-bold text-white transition-colors duration-200 hover:bg-blue-600"
@@ -27,7 +111,16 @@ export default function Home() {
             <PrinterIcon className="h-5 w-5" />
           </button>
         </div>
-      </nav>
+      </nav> */}
+
+      <div className="w-12 h-12 fixed bottom-8 right-8">
+        <button
+          onClick={handlePrint}
+          className="flex items-center justify-center w-full h-full bg-white rounded-full shadow outline-none focus-visible:ring text-gray-600"
+        >
+          <DownloadIcon />
+        </button>
+      </div>
 
       <div className="mx-auto max-w-4xl space-y-4 p-0 py-4 px-2 md:py-10">
         <div className="shadow">
@@ -82,130 +175,38 @@ const CV = forwardRef<HTMLDivElement>((props, ref) => {
       </div>
 
       <div className="bg-white p-6">
-        <p className="line-clamp-5 text-sm leading-relaxed text-gray-700">
-          {/* Front-End Engineer with 5 years of experience with detailed understanding in developing
-          modern and responsive interfaces for a wide range of businesses. Excellent grasp of React,
-          Nextjs, TS, Redux, Best Practices, Testing, REST and GraphQL. Also starting to get more
-          familiarized with ReactNative and Python */}
-          Software Engineer with expertise in Front-End development. Over 5 years of experience
-          developing modern and performant web applications for a wide range of businesses. Well
-          familiar with Javascript, TypeScript, React and studying React Native and Node. Enjoys
-          spending time learning and sharing knowledge with the community.
-        </p>
+        <p className="line-clamp-5 text-sm leading-relaxed text-gray-700">{description}</p>
         {/* Work history */}
-        <div className="mt-4 border-t-2 border-b-2 border-gray-100 py-1">
-          <h3 className="text-lg font-bold text-blue-500">Work History</h3>
+        <div className="mt-4 grid gap-4">
+          <div className="border-t-2 border-b-2 border-gray-100 py-1">
+            <h3 className="text-lg font-bold text-blue-500">Work History</h3>
+          </div>
+          {experiences.map(({ startAt, endAt, role, company, location, description }, i) => (
+            <div key={i} className="grid grid-cols-4">
+              <div className="col-span-1 text-sm text-gray-700">
+                <div>{dayjs(startAt).format("MMM YYYY")} - </div>
+                <div>{endAt ? dayjs(endAt).format("MMM YYYY") : "Present"}</div>
+                <div className="mt-1 text-xs text-gray-500">
+                  {getWorkingTimeFormatted(startAt, endAt)}
+                </div>
+              </div>
+              <div className="col-span-3">
+                <h4 className="text-lg font-bold text-gray-800">{role}</h4>
+                <p className="text-sm italic text-gray-700">
+                  {company}, {location}
+                </p>
+                <ul className="ml-4 list-disc">
+                  {description.map((d, i) => (
+                    <li key={i} className="text-sm leading-normal text-gray-700">
+                      {d}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          ))}
         </div>
-        <div className="mt-4 grid grid-cols-4 gap-y-4">
-          {/* Work #1 */}
-          <div className="col-span-1 text-sm text-gray-700">
-            <div>2022-07 - </div>
-            <div>Present</div>
-            <div className="mt-1 text-xs text-gray-500">1yr 3mos</div>
-          </div>
-          <div className="col-span-3">
-            <h4 className="text-lg font-bold text-gray-800">Full-Stack Developer</h4>
-            <p className="text-sm italic text-gray-700">Suvie, US (remote)</p>
-            <ul className="ml-4 list-disc">
-              <li className="text-sm leading-normal text-gray-700">
-                Refactoring company&apos;s site from CSR to SSG. SEO and lighthouse scores were
-                hugely improved
-              </li>
-              <li className="text-sm leading-normal text-gray-700">
-                Merge various projects into a monorepo (Turborepo) to allow code sharing. Resulted
-                in better DX and DRY code
-              </li>
-              <li className="text-sm leading-normal text-gray-700">
-                Maintenance of mobile app built in React Native
-              </li>
-              <li className="text-sm leading-normal text-gray-700">
-                Add feature flags for marketing strategies and conversion
-              </li>
-            </ul>
-          </div>
 
-          {/* Work #1 */}
-          <div className="col-span-1 text-sm text-gray-700">
-            <div>2020-11 - </div>
-            <div>2022-05</div>
-            <div className="mt-1 text-xs text-gray-500">1yrs 7mos</div>
-          </div>
-          <div className="col-span-3">
-            <h4 className="text-lg font-bold text-gray-800">Front-End Developer</h4>
-            <p className="text-sm italic text-gray-700">SweetRush Inc., US (remote)</p>
-            <ul className="ml-4 list-disc">
-              <li className="text-sm leading-normal text-gray-700">
-                Refactoring a Shopify store to use Hydrogen React
-              </li>
-              <li className="text-sm leading-normal text-gray-700">
-                Development of landing pages for marketing campaigns
-              </li>
-              <li className="text-sm leading-normal text-gray-700">
-                Migration of site from WordPress to Headless WP + Next.js
-              </li>
-            </ul>
-          </div>
-
-          {/* Work #1 */}
-          <div className="col-span-1 text-sm text-gray-700">
-            <div>2019-07 - </div>
-            <div>2020-08</div>
-            <div className="mt-1 text-xs text-gray-500">11mos</div>
-          </div>
-          <div className="col-span-3">
-            <h4 className="text-lg font-bold text-gray-800">Front-End Developer</h4>
-            <p className="text-sm italic text-gray-700">UpWork, (freelancer)</p>
-            <ul className="ml-4 list-disc">
-              <li className="text-sm leading-normal text-gray-700">
-                Development of various sites and applications
-              </li>
-              <li className="text-sm leading-normal text-gray-700">
-                Migration of a legacy app built with Redux and complex state to a simpler
-                architecture in ReactQuery
-              </li>
-              <li className="text-sm leading-normal text-gray-700">
-                Performance improvement by replacing MUI with Tailwind + Radix. Lighhouse scores
-                more than doubled
-              </li>
-            </ul>
-          </div>
-
-          {/* Work #2 */}
-          <div className="col-span-1 text-sm text-gray-700">
-            <div>2017-04 - </div>
-            <div>2019-07</div>
-            <div className="mt-1 text-xs text-gray-500">2yrs 4mos</div>
-          </div>
-          <div className="col-span-3">
-            <h4 className="text-lg font-bold text-gray-800">Full Stack Desktop Developer</h4>
-            <p className="text-sm italic text-gray-700">Prevedello Sistemas, Brazil (on-site)</p>
-            <ul className="ml-4 list-disc">
-              <li className="text-sm leading-normal text-gray-700">
-                Maintenance and development of ERP and POS systems
-              </li>
-              <li className="text-sm leading-normal text-gray-700">
-                API integrations with warehouse and e-commerces
-              </li>
-            </ul>
-          </div>
-
-          {/* Work #3 */}
-          <div className="col-span-1 text-sm text-gray-700">
-            <div>2016-03 - </div>
-            <div>2017-04</div>
-            <div className="mt-1 text-xs text-gray-500">1yr 2mos</div>
-          </div>
-          <div className="col-span-3">
-            <h4 className="text-lg font-bold text-gray-800">Technical Support Analyst</h4>
-            <p className="text-sm italic text-gray-700">Prevedello Sistemas, Brazil (on-site)</p>
-            <ul className="ml-4 list-disc">
-              <li className="text-sm leading-normal text-gray-700">
-                Evaluate user problems, respond to support requests and patiently walk individuals
-                through basic troubleshooting tasks
-              </li>
-            </ul>
-          </div>
-        </div>
         {/* Education history */}
         <div className="mt-4 border-t-2 border-b-2 border-gray-100 py-1">
           <h3 className="text-lg font-bold text-blue-500">Education</h3>
